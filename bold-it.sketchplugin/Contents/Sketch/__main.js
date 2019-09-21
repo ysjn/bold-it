b88e0578aa1764870a77f8a258a249dfc3206b93
ysjn <jun.yoyoyoshida@gmail.com>
2019-09-22 04:43:43 +0900

    update @skpm/builder

---
+++ bold-it.sketchplugin/Contents/Sketch/__main.js
@@ -0,0 +1,205 @@
+var globalThis = this;
+var global = this;
+function __skpm_run (key, context) {
+  globalThis.context = context;
+  try {
+
+var exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! sketch */ "sketch");
+/* harmony import */ var sketch__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(sketch__WEBPACK_IMPORTED_MODULE_0__);
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  var doc = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.getSelectedDocument();
+  var selectedLayers = doc.selectedLayers;
+  var selectedCount = selectedLayers.length;
+
+  if (selectedCount == 0) {
+    sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message('Please select a layer.');
+  } else {
+    var inputText = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.getStringFromUser("Enter a text you want to bold", 'text');
+
+    if (inputText.trim() == '') {
+      sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message('Please enter a text.');
+      return false;
+    }
+
+    var selection = context.selection;
+
+    for (var i = 0; i < selectedCount; i++) {
+      var layer = selection[i];
+      var layerType = layer.class();
+
+      if (layerType == 'MSTextLayer') {
+        var textValue = layer.stringValue();
+        var textFont = NSFontManager.sharedFontManager().convertFont_toHaveTrait(layer.font(), 2);
+
+        if (textFont.traits() != 2) {
+          sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message('Error: This font seems to have no bold weight 😢');
+          return false;
+        }
+
+        var ranges = getRanges(textValue, inputText);
+
+        if (ranges.length < 1) {
+          sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message('Error: No matching text found 😢');
+          return false;
+        }
+
+        layer.setIsEditingText(true);
+
+        for (var idx = 0; idx < ranges.length; idx++) {
+          layer.addAttribute_value_forRange_(NSFontAttributeName, textFont, ranges[idx]);
+        }
+
+        layer.setIsEditingText(false);
+      }
+    }
+
+    sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("Bolded ".concat(ranges.length, " ").concat(ranges.length > 1 ? 'texts' : 'text', "\uFF01 \u2728"));
+  }
+});
+
+function getRanges(str, query) {
+  var len = query.length;
+  var ranges = [];
+  var idx = 0;
+  var lastIdx = 0;
+
+  while (str.indexOf(query, lastIdx) > -1) {
+    idx = str.indexOf(query, lastIdx);
+    ranges.push(NSMakeRange(idx, len));
+    lastIdx = idx + len;
+  }
+
+  return ranges;
+}
+
+/***/ }),
+
+/***/ "sketch":
+/*!*************************!*\
+  !*** external "sketch" ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("sketch");
+
+/***/ })
+
+/******/ });
+    if (key === 'default' && typeof exports === 'function') {
+      exports(context);
+    } else if (typeof exports[key] !== 'function') {
+      throw new Error('Missing export named "' + key + '". Your command should contain something like `export function " + key +"() {}`.');
+    } else {
+      exports[key](context);
+    }
+  } catch (err) {
+    if (typeof process !== 'undefined' && process.listenerCount && process.listenerCount('uncaughtException')) {
+      process.emit("uncaughtException", err, "uncaughtException");
+    } else {
+      throw err
+    }
+  }
+}
+globalThis['onRun'] = __skpm_run.bind(this, 'default')
+
+//# sourceMappingURL=__main.js.map
